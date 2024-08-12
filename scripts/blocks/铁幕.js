8196ff0b415f274d762b89c86e20b0ba31140e36
Cry0flu1d <3462772994@qq.com>
2024-08-12 20:22:23 +0800

    3.2.1.1

--- scripts/blocks/铁幕.js
+++ scripts/blocks/铁幕.js
@@ -1,6 +1,6 @@
 const myitems = require("SFitems");
 
 const 铁幕 =extend(ForceProjector,"铁幕",{});
-铁幕.phaseUseTime = 180;
-铁幕.phaseShieldBoost = 4000;
+铁幕.phaseUseTime = 120;
+铁幕.phaseShieldBoost = 3600;
 铁幕.itemConsumer=铁幕.consumeItems(ItemStack.with(myitems.泰勒合金,1)).boost();
